029c4872c08728be0d812c818d33d9b9648e6a1a
AR <AnastasiaR@gmail.com>
2024-09-19 00:21:11 +0300

    creating home page

--- dndCharacterList/dndCharacterList/Controllers/HomeController.cs
+++ dndCharacterList/dndCharacterList/Controllers/HomeController.cs
@@ -15,7 +15,18 @@ namespace dndCharacterList.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var info = new ProjectInfo(
+                "Анастасія Рибчинська",
+                "ІПЗс-24",
+                "Онлайн-ресурс конструктор для створення персонажа DnD",
+                "");
+            //{
+            //    Name = "Анастасія",
+            //    Group = "SE-2024",
+            //    
+            //    ThemeDescription = "This course work explores the importance of automated testing, its types, and implementation in large-scale software projects."
+            //};
+            return View(info);
         }
 
         public IActionResult Privacy()
---
+++ dndCharacterList/dndCharacterList/Models/ProjectInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace dndCharacterList.Models
+{
+    public class ProjectInfo
+    {
+        public ProjectInfo(){
+        
+        }
+        public ProjectInfo(string name, string group, string theme, string themeDescription) {
+
+            Name = name;
+            Group = group;
+            Theme = theme;
+            ThemeDescription = themeDescription;
+        }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public string Theme { get; set; }
+        public string ThemeDescription { get; set; }
+       
+
+    }
+}
